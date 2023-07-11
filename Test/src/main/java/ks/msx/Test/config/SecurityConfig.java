52d5a4f11e8be809042f387136929fd0edaf0149
Cresciuc Maxim <cresciucmax@gmail.com>
2023-07-11 12:07:02 +0300

    main

--- Test/src/main/java/ks/msx/Test/config/SecurityConfig.java
+++ Test/src/main/java/ks/msx/Test/config/SecurityConfig.java
@@ -1,20 +1,20 @@
 package ks.msx.Test.config;
 
 
-import jakarta.servlet.FilterChain;
-import ks.msx.Test.entity.Authority;
 import ks.msx.Test.entity.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -41,22 +41,21 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService(){
         UserDetails user = User
                 .withUsername("user")
-                .password("user")
+                .password(passwordEncoder().encode("user"))
                 .roles(Role.USER.getAuthority())
                 //.authorities(Role.USER.getAuthority())
                 .build();
         UserDetails admin = User
                 .withUsername("admin")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 //.authorities(Role.ADMIN.getAuthority())
                 .roles(Role.ADMIN.getAuthority())
                 .build();
-        //System.out.println(Role.USER.getAuthority());
         return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(12);
     }
 }
